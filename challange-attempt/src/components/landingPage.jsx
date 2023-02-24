bee33ec1a36e7c48a8a92b21e7431e42af7d1b49
MikeProduce <105679025+MikeProduce@users.noreply.github.com>
2023-02-24 09:23:46 -0600

    made most of the other sections responsive to desktop view just need the footer and the svg for the backgrounds

--- challange-attempt/src/components/landingPage.jsx
+++ challange-attempt/src/components/landingPage.jsx
@@ -8,16 +8,16 @@ export const LandingPage = () => {
 //sm: 640,md:768,lg:1024;,xl:1280
 
     return (
-        <section className='bg-Very-Light-Gray pb-10 md:p-20 overflow-hidden md:overflow-visible'>
-            <div className='lg:mx-20 md:flex md:flex-row-reverse'>
+        <section className='bg-Very-Light-Gray pb-10 md:py-20 md:px-14 lg:px-5 overflow-hidden md:overflow-visible'>
+            <div className='lg:mx-20 md:flex md:flex-row-reverse md:py-32'>
             <div className='w-full h-full'>
-            <img className='z-2 absolute -top-0 left-0 sm:-top-70  xxs:-top-20 xs:-top-30 smm:-top-30' src={MobilePhones} alt={MobilePhones}/>
+            <img className='z-2 absolute -top-0 left-0 sm:-top-70  xxs:-top-20 xs:-top-30 smm:-top-30 md:hidden' src={MobilePhones} alt={MobilePhones}/>
             <img className='z-1 absolute top-0 hidden' src={DesktopImage} alt={DesktopImage} />
             <img className='md:hidden w-full' src={MobileImage} alt="" />
             </div>
-            <div className='text-center items-center'>
-                <h1 className='text-4xl p-4'>Next generation digital banking</h1>
-                <p className='p-4 text-sm text-Grayish-Blue'>Take your financial life online. Your Easybank account wil be a one-stop-shop for spending,saving,budgeting,investing, and much more.
+            <div className='text-center items-center md:text-left'>
+                <h1 className='text-4xl p-4 md:py-4 md:px-0 '>Next generation digital banking</h1>
+                <p className='p-4 text-sm text-Grayish-Blue md:py-4 md:px-0'>Take your financial life online. Your Easybank account wil be a one-stop-shop for spending,saving,budgeting,investing, and much more.
                 </p>
                 <button className="rounded-3xl px-4 py-3 bg-gradient-to-r from-lime-green to-Bright-Cyan text-white text-md">Request Invite</button>
             </div>
--- challange-attempt/src/components/sectionOne.jsx
+++ challange-attempt/src/components/sectionOne.jsx
@@ -5,40 +5,42 @@ import OpenApi from '../images/icon-api.svg'
 
 
 export const SectionOne = () => {
+    const features = [{
+        icon: OnlineBanking,
+        title: 'Online Banking',
+        summary:'Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world.'
+    },{
+        icon: SimpleBudgeting,
+        title: 'Simple Budgeting',
+        summary:"See exactly where your money goes each month. Receive notifcations when you're close to your hitting limits."
+    },{
+        icon: FastOnboard,
+        title: 'Fast Onboarding',
+        summary:"We don't do branches. Open your account in minutes online and start taking control of your finances right away."
+    },{
+        icon: OpenApi,
+        title: 'Open API',
+        summary:"Manage your savings, investments, pension, and much more from one account.Tracking your money has never been easier."
+    }]
 
 
 
     return (
-        <section className='bg-Light-Grayish-Blue pb-10 pt-10 md:z-10 lg:z-10'>
-            <div className='lg:mx-20'>
-            <div className='text-center'>
-                <h1 className='text-3xl p-4'>Why choose <br/> Easybank?</h1>
-                <p className='p-4 text-sm text-Grayish-Blue'>We leverage Open Banking to turn your bank account into your financial hub. Control your finances like never before.
+        <section className='bg-Light-Grayish-Blue pb-10 pt-10 md:z-10 lg:z-10 md:p-4 md:py-32'>
+            <div className='lg:mx-20 md:mx-10'>
+            <div className='text-center md:text-left'>
+                <h1 className='text-3xl p-4 md:p-2'>Why choose <br className='md:hidden'/> Easybank?</h1>
+                <p className='p-4 md:p-2 md:pb-10 text-sm text-Grayish-Blue lg:w-2/4 md:text-lg'>We leverage Open Banking to turn your bank account into your financial hub. Control your finances like never before.
                 </p>
             </div>
-            <div className='text-center p-4'>
-                <img className='mx-auto' src={OnlineBanking} alt="" />
-                <h1 className='text-2xl p-4'>Online Banking</h1>
-                <p className='p-4 text-sm text-Grayish-Blue'>Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world.
-                </p>
+            <div className='md:flex'>
+            {features.map((feature) => (
+             <div className='text-center p-4 md:text-left md:p-2'>
+                <img className='mx-auto md:mx-0 md:w-50' src={feature.icon} alt="" />
+                <h1 className='text-2xl p-4 md:text-lg md:py-2 md:px-0'>{feature.title}</h1>
+                <p className='p-4 text-sm text-Grayish-Blue md:text-smm md:px-0'>{feature.summary}</p>
             </div>
-            <div className='text-center p-4'>
-                <img className='mx-auto' src={SimpleBudgeting} alt="" />
-                <h1 className='text-2xl p-4'>Simple Budgeting</h1>
-                <p className='p-4 text-sm text-Grayish-Blue'>See exactly where your money goes each month. Receive notifcations when you're close to your hitting limits.
-                </p>
-            </div>
-            <div className='text-center p-4'>
-                <img className='mx-auto' src={FastOnboard} alt="" />
-                <h1 className='text-2xl p-4'>Fast Onboarding</h1>
-                <p className='p-4 text-sm text-Grayish-Blue'>We don't do branches. Open your account in minutes online and start taking control of your finances right away.
-                </p>
-            </div>
-            <div className='text-center p-4'>
-                <img className='mx-auto' src={OpenApi} alt="" />
-                <h1 className='text-2xl p-4'>Open API</h1>
-                <p className='p-4 text-sm text-Grayish-Blue'>Manage your savings, investments, pension, and much more from one account.Tracking your money has never been easier.
-                </p>
+                ))}
             </div>
             </div>
         </section>
